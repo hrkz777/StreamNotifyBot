9986d8cb6168c5f9c3315293f751c15884a44829
hrkz777 <62292427+hrkz777@users.noreply.github.com>
2026-09-01 17:56:29 +0900

    fix: PHPUnitの実行環境をtestに固定

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,6 +6,10 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
+$_SERVER['APP_ENV'] = 'test';
+$_ENV['APP_ENV'] = 'test';
+putenv('APP_ENV=test');
+
 (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 
 if ((bool) $_SERVER['APP_DEBUG']) {
