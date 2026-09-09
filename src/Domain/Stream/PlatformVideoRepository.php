270712da6f5e535b8f714ec612f11949b20a797e
hrkz777 <62292427+hrkz777@users.noreply.github.com>
2026-09-09 18:19:35 +0900

    feat: 保存済みプラットフォーム動画を検索可能にする

--- src/Domain/Stream/PlatformVideoRepository.php
+++ src/Domain/Stream/PlatformVideoRepository.php
@@ -8,4 +8,6 @@ interface PlatformVideoRepository
 {
     /** Returns the ID of the persisted video, including an existing matching row. */
     public function save(PlatformVideo $video): string;
+
+    public function findById(string $id): ?PlatformVideo;
 }
